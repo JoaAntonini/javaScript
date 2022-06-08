7294e492d0984adad008e616be8c444166155289
Johanna Antonini <antonini.johanna@gmail.com>
2022-06-07 21:59:19 -0300

    entrega optimizacion

--- script1.js
+++ script1.js
@@ -1,16 +1,42 @@
 
+//Armo la lista de lineas de credito disponibles
+
+const listaLineas = [ "Adquisición Vivienda", "Construcción"];
+console.log(listaLineas);
+
+//Agrego una nueva linea al final (push) porque en orden de prioridad (monto a prestar) es la menos importante
+
+listaLineas.push("Construcción con Terreno");
+console.log(listaLineas);
+
+//elimino la linea adquisición porque voy a generar 2 lineas una para vivienda usada y una para nueva
+
+listaLineas.shift();
+
+listaLineas.unshift("Adquisición Vivienda Nueva","Adquisición Vivienda Usada");
+
+//verifico como queda mi lista final
+
+console.log(listaLineas);
+
 // Dejo en la consola la lista de lineas y condiciones para luego impactarlas en el HTML y que el cliente pueda saber 
 // y elegir cual le interesa y si califica, para luego ingresar en el calculador y saber cual seria su cuota
+//sumo lista de documentacion requerida 
 
 const listaCondiciones=[
     {
         nombre: "Adquisición Vivienda Nueva",
-        montoMaximo: 10000000,
+        montoMaximo: 10000000,        
         ingresoMinimo: 40000,
         edadMinima: 18,
         edadMaxima: 70,
         plazoMinimoMeses: 120,
-        plazoMaximaMeses: 240
+        plazoMaximoMeses: 240,
+        img:"./img/compraviviendanueva.jpg",
+        dni:"DNI",
+        comprobanteDomicilio: "comprobanteDomicilio",
+        recibosDeSueldo: "recibosDeSueldo",
+        docTecnica: "Escritura y Boleto Compra-Venta"
     },
     {
         nombre: "Adquisición Vivienda Usada",
@@ -19,7 +45,12 @@ const listaCondiciones=[
         edadMinima: 18,
         edadMaxima: 70,
         plazoMinimoMeses: 120,
-        plazoMaximaMeses: 240
+        plazoMaximoMeses: 240,
+        img:"./img/compraviviendausada-min.jpg",
+        dni: "DNI",
+        comprobanteDomicilio: "comprobanteDomicilio",
+        recibosDeSueldo: "recibosDeSueldo",
+        docTecnica: "Escritura y Boleto Compra-Venta"
     },
     {
         nombre: "Construcción",
@@ -28,7 +59,12 @@ const listaCondiciones=[
         edadMinima: 18,
         edadMaxima: 70,
         plazoMinimoMeses: 120,
-        plazoMaximaMeses: 240
+        plazoMaximoMeses: 240,
+        img: "./img/construccion-min.jpg",
+        dni: "DNI",
+        comprobanteDomicilio: "comprobanteDomicilio",
+        recibosDeSueldo: "recibosDeSueldo",
+        docTecnica: "Escritura, plano y Proyecto de Obra"
     },
     {
         nombre: "Construcción con Terreno",
@@ -37,36 +73,14 @@ const listaCondiciones=[
         edadMinima: 18,
         edadMaxima: 70,
         plazoMinimoMeses: 120,
-        plazoMaximaMeses: 240
+        plazoMaximoMeses: 240,
+        img: "./img/construccionconterreno-min.jpg",
+        dni: "DNI",
+        comprobanteDomicilio: "comprobanteDomicilio",
+        recibosDeSueldo: "recibosDeSueldo",
+        docTecnica : "Escritura, plano y Proyecto de Obra"
     },
-        {
-        nombre: "Refacción",
-        montoMaximo: 100000,
-        ingresoMinimo: 20000,
-        edadMinima: 18,
-        edadMaxima: 70,
-        plazoMinimoMeses: 48,
-        plazoMaximaMeses: 96
-    },
-    {
-        nombre: "Ampliación",
-        montoMaximo: 3000000,
-        ingresoMinimo: 50000,
-        edadMinima: 18,
-        edadMaxima: 70,
-        plazoMinimoMeses: 48,
-        plazoMaximaMeses: 120
-    },
-    {
-        nombre: "Materiales",
-        montoMaximo: 500000,
-        ingresoMinimo: 20000,
-        edadMinima: 18,
-        edadMaxima: 70,
-        plazoMinimoMeses: 24,
-        plazoMaximaMeses: 60
-    },
-];
+ ];
 
 
 //pongo en el HTML las lineas que tengo disponibles
@@ -75,7 +89,6 @@ const listaCondiciones=[
 let lineas = document.createElement("div");
 lineas.className="card textCenter alineaCards";
 
-
     //Para cada linea del array voy a tener una card
     for (const linea of listaCondiciones){
         let card = document.createElement("div");
@@ -83,10 +96,10 @@ lineas.className="card textCenter alineaCards";
             <div class="container-fluid">
                 <div class="row">
                     <div class="alineaCards  margenCards">
-                        <div class="col-lg-12 ">
-                            <div class="card">
+                            <div class="card card-img-top">
                                 <div class="card-body">
-                                    <p class="card-title">${linea.nombre}</p>
+                                    <p class="card-title tamañoTitulos2">${linea.nombre}</p>
+                                    <img src="${linea.img}" class="card-img-top" alt="${linea.nombre}">
                                 </div>
                             </div>
                          </div>
@@ -97,8 +110,26 @@ lineas.className="card textCenter alineaCards";
         lineas.appendChild(card);
     }
 
+//desestructuro el array de lineas para quedarme con las 2 recomendadas del momento
 
 let insertaLineas=document.getElementById("lineas");
+const p = document.createElement("p");
+const p2 = document.createElement("p");
+const p3 = document.createElement("p");
+const h3 = document.createElement("h3");
+const [a,,b] = listaLineas;
+h3.innerText = `Lineas recomendadas del mes`
+p2.innerText = a
+p3.innerText = b
+p.innerHTML = `Tenemos ${listaLineas.length} lineas de creditos disponibles para ofrecerte`
+p.className= "textCenter tamañoTitulos";
+h3.className= "textCenter tamañoTitulos2";
+p2.className= "textCenter";
+p3.className= "textCenter";
+insertaLineas.appendChild(p);
+insertaLineas.appendChild(h3);
+insertaLineas.appendChild(p2);
+insertaLineas.appendChild(p3);
 insertaLineas.appendChild(lineas);
 
 //le inserto formato al titulo de condiciones y le cambio el texto
@@ -127,11 +158,41 @@ let lista = document.createElement("div");
                     </div>
                     <ul>
                         <li class="card-text alineaInfo">Edad entre ${producto.edadMinima} y ${producto.edadMaxima}</li>
-                        <li class="card-text alineaInfo">Plazo ${producto.plazoMinimoMeses} / ${producto.plazoMaximoMeses}meses</li>
+                        <li class="card-text alineaInfo">Plazo ${producto.plazoMinimoMeses} & ${producto.plazoMaximoMeses} meses</li>
                         <li class="card-text alineaInfo">Financiación hasta $ ${producto.montoMaximo}</li>
                         <li class="card-text alineaInfo">Ingreso minimo $ ${producto.ingresoMinimo}</li>
                     </ul>
-                    <button>Más info</button>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Modal1">
+                    Mas info
+                    </button>
+                    <!-- Modal -->
+                        <div class="modal fade" id="Modal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                          <div class="modal-dialog">
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Requisitos y Documentacion ${producto.nombre}</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                              </div>
+                              <div class="modal-body">
+                               <ul>
+                                    <li class="card-text alineaInfo">Edad Minima ${producto.edadMinima}</li>
+                                    <li class="card-text alineaInfo">Edad Maxima ${producto.edadMaxima}</li>
+                                    <li class="card-text alineaInfo">Plazo Minimo ${producto.plazoMinimoMeses}  meses</li>
+                                    <li class="card-text alineaInfo">Plazo Maximo ${producto.plazoMaximoMeses} meses</li>
+                                    <li class="card-text alineaInfo">Financiación hasta $ ${producto.montoMaximo}</li>
+                                    <li class="card-text alineaInfo">Ingreso minimo $ ${producto.ingresoMinimo}</li>
+                                    <li class="card-text alineaInfo"> Documentacion Personal: ${producto.dni}</li>
+                                    <li class="card-text alineaInfo"> Documentacion Personal2: ${producto.comprobanteDomicilio}</li>
+                                    <li class="card-text alineaInfo"> Documentacion Laboral:  ${producto.recibosDeSueldo}</li>        
+                                    <li class="card-text alineaInfo"> Documentacion Tecnica:  ${producto.docTecnica}</li>  
+                                </ul>
+                              </div>
+                              <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
                 </div>
             </div>
             </div>
@@ -150,51 +211,129 @@ lista.className= "alineaCards";
 
 //pedimos al cliente que ingrese la linea que le interesa y calcule su cuota
 
-function calcular(capital, Interes, anos) {
+let boton = document.getElementById("miBoton");
 
-    let cuota = ((capital / anos / 12) * (1 + Interes)).toFixed(2);
 
-    alert("Para un prestamo de " + capital + " a " + (anos * 12) + " meses y una tasa del 20%, su cuota mensual sera de " + cuota);
-
-    document.write("<h4> La cuota Mensual sera de " + cuota + "</h4>");
-
+boton.onmouseover = () =>{
+    boton.className="btn btn-primary";
 }
 
+boton.onmouseout = () => {
+    boton.className="btn btn-dark";
+}
 
-for (let i = 1; i <= 10; i++) {
+/*if (!ingreso || !antiguedad || !capital || !plazo) {
+        alert("Por favor, complete los campos");
+        return;
+    }*/
 
-    let IngresoNetoFamiliar = parseInt(prompt("Ingreso Neto Familiar"));
+function calcular (e){
+    e.preventDefault();
+    let done=0;
+    let ingreso = document.getElementById("ingreso").value;
+    let antiguedad = document.getElementById("antiguedad").value;
+    let capital = document.getElementById("capital").value;
+    let plazo = document.getElementById("plazo").value;
+    formulario.reset();
+    //incorporo operador AND
+    (!ingreso || !antiguedad || !capital || !plazo) && alert("Por favor, complete los campos");
+    /*if (!ingreso || !antiguedad || !capital || !plazo) {
+        alert("Por favor, complete los campos");
+        return;
+    }*/
 
-    let AntiguedadLaboral = parseInt(prompt("Antiguedad Laboral"));
+    let msj = "";
 
-    if (IngresoNetoFamiliar < 20000 && AntiguedadLaboral < 3) {
-
-        alert("No califica para esta linea");
-
-        break;
-
+    if (ingreso < 20000 && antiguedad < 3) {
+        msj ="No califica para esta linea";
+    }else if (ingreso < 1000000 && antiguedad >= 3){
+        let cuota = ((capital / plazo / 12) * (1.2)).toFixed(2);
+        msj= "La cuota mensual sera de " + cuota;
     }
-
-    else if (IngresoNetoFamiliar >= 20000 && IngresoNetoFamiliar < 1000000 && AntiguedadLaboral >= 3) {
-
-        let capital = prompt("Introduzca el monto del prestamo a solicitar \n Monto minimo 2000000\n Monto máximo 10000000");
-
-        let Interes = 0.2;
-
-        let anos = prompt("Lo va a pagar en 10 o 20 años");
-
-        calcular(capital, Interes, anos);
-
-        break;
-
-    }
-
     else {
-
-        alert("Comunicate con un representate para revisar tu casa");
-
-        break;
-
+        msj = "Comunicate con un representate para revisar tu caso";
     }
+    let h4 = document.createElement("h4");
+    h4.innerHTML = msj;
+    document.getElementById("oferta").prepend(h4);
+    localStorage.setItem("cuota", cuota.value);
+};
 
+//Controla que haya completado datos del formulario (sean numero y no esten vacios)
+
+let formulario = document.getElementById("formulario");
+formulario.addEventListener("submit", calcular);
+
+
+//Login para que le quede registrada la cuota que calculo (me sale como undefined, porque?)
+//guarda los datos pero los pierdo cuando refresco la sesion (porque?)
+//no me deja guardar mas de un usuario  (como sumo mas usuarios?)
+
+
+ function store(){
+    let usuario=document.getElementById("nombre").value;
+    let email=  document.getElementById("email").value;
+    let password=  document.getElementById("password").value;
+     let inputUsuario= document.getElementById("nombre");
+     localStorage.setItem("nombre", inputUsuario.value);
+     let inputPassword= document.getElementById("password");
+     localStorage.setItem("password", inputPassword.value);
+     let inputEmail= document.getElementById("email");
+     localStorage.setItem("email", inputEmail.value);     
+     (usuario=="" || email=="" || password=="")? alert("Por favor ingrese, nombre, email y contraseña") : document.getElementById("login").submit();
 }
+
+//reemplazo en linea 263 if con operador ternario 
+//el return esta implicito
+/*if (usuario=="" || email=="" || password=="") {
+        alert("Por favor ingrese, nombre, email y contraseña");
+        return;
+    }else{ 
+        document.getElementById("login").submit();
+    }*/
+
+
+
+//genero con un spread de objetos la informacion financiera de las lineas
+//guardo objeto con JSON para mostrar
+const linea1 = {nombre:"Adquisición Vivienda Nueva", tasa: 15, CFT: 60 };
+const linea1Json = JSON.stringify(linea1);
+
+console.log(linea1Json);
+const linea2 = {...linea1, nombre:"Adquisición Vivienda Usada"};
+const linea2Json = JSON.stringify(linea2);
+const linea3 = {...linea1,nombre:"Construcción", tasa: 10};
+const linea3Json = JSON.stringify(linea3);
+const linea4 = {...linea3,nombre:"Construcción con Terreno", CFT: 55};
+const linea4Json = JSON.stringify(linea4);
+
+let condicionesFinancieras=document.getElementById("financiacion");
+
+const fp1 = document.createElement("p");
+const fp2 = document.createElement("p");
+const fp3 = document.createElement("p");
+const fp4 = document.createElement("p");
+const fh3 = document.createElement("h3");
+
+fh3.innerText = `Condiciones Financieras`
+fp1.innerText = linea2Json
+fp2.innerText = linea2Json
+fp3.innerText = linea3Json
+fp4.innerText = linea4Json
+
+fh3.className= "textCenter tamañoTitulos2";
+
+condicionesFinancieras.appendChild(fh3);
+condicionesFinancieras.appendChild(fp1);
+condicionesFinancieras.appendChild(fp2);
+condicionesFinancieras.appendChild(fp3);
+condicionesFinancieras.appendChild(fp4);
+//condicionesFinancieras.appendChild(financiacion);
+
+
+//Pendientes
+
+//modal BS para las condiciones de cada producto ok, pero me vincula a todos las mismas condiciones.
+//"me interesa" disparar correo para contactar al cliente
+//le da calcular y acumula los resultados en el mensaje del html (reset no sirve)
+//calcular intereses del array
